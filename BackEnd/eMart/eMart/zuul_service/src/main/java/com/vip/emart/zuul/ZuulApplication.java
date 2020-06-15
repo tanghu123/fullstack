e4306fb4a18777b1934dabe97b691f050a419f26
HuTang <tang_hu123@163.com>
2020-06-16 00:12:49 +0800

    add zuul api gateway

---
+++ BackEnd/eMart/eMart/zuul_service/src/main/java/com/vip/emart/zuul/ZuulApplication.java
@@ -0,0 +1,15 @@
+package com.vip.emart.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication(scanBasePackages = "com.vip.emart.*")
+@EnableZuulProxy
+public class ZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+}
