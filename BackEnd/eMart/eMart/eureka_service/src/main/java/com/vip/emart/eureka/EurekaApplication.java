ca53a4bc5f5fa4e0eb42cfdcb3c2804bd335e3f3
HuTang <tang_hu123@163.com>
2020-06-15 19:24:16 +0800

    add eureka

--- BackEnd/eMart/eMart/eureka_service/src/main/java/com/vip/emart/eureka/EurekaApplication.java
+++ BackEnd/eMart/eMart/eureka_service/src/main/java/com/vip/emart/eureka/EurekaApplication.java
@@ -1,15 +1,15 @@
-package com.vip.emart.user;
+package com.vip.emart.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication(scanBasePackages = "com.vip.emart.*")
-@EnableEurekaClient
-public class UserApplication {
+@EnableEurekaServer
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
